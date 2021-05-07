ddb460f64ec8ffe63cf95082744a537fb9e308ac
tomasfilipe7 <tomasfilipe7@ua.pt>
2021-05-07 16:37:03 +0100

    Application template

---
+++ dubaidash/src/main/java/com/project/dubaidash/DubaidashApplication.java
@@ -0,0 +1,13 @@
+package com.project.dubaidash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubaidashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubaidashApplication.class, args);
+	}
+
+}
