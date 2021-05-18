b4c3a5874581f2f55050aef8f29f2d3b7dee3256
franciscoxaviersp <45402958+franciscoxaviersp@users.noreply.github.com>
2021-05-18 17:04:28 +0100

    Update build.sh

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -8,6 +8,6 @@ for i in ${!MODULES[@]}; do
 	cd ${MODULES[$i]}
 
 	rm -rf target
-	mvn -X -Dmaven.test.skip=true deploy
+	mvn -X -Dmaven.test.skip=true --settings ../settings.xml deploy
 	cd ..
 done
