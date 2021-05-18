3e53c0ae4341dc9348bd6e475d78fab3cc027df5
danielvcorreia <danielvalacorreia@ua.pt>
2021-05-19 00:28:30 +0100

    Added markers

--- dubaidash-react-app/src/components/MapBox.js
+++ dubaidash-react-app/src/components/MapBox.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import mapboxgl from 'mapbox-gl';
 import '../App.css';
+import AirplaneService from '../services/AirplaneService';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuaWVsdmNvcnJlaWEiLCJhIjoiY2tpa2xodTViMGIyZjJxcGtsNHZkYnU4NCJ9.xk7aj9lSbWNLpvwM_HmdEg';
 
 class MapBox extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-          lng: 55.36,
-          lat: 25.25,
-          zoom: 10
-        };
-        this.mapContainer = React.createRef();
-    }
+  constructor(props) {
+      super(props);
+      this.state = {
+        lng: 55.36,
+        lat: 25.25,
+        zoom: 10
+      };
+      this.mapContainer = React.createRef();
+  }
 
-    componentDidMount() {
+  async componentDidMount() {
+        var data = await AirplaneService.get_dubai_airplanes();
+        data = data.data;
+
         const { lng, lat, zoom } = this.state;
         const map = new mapboxgl.Map({
           container: this.mapContainer.current,
@@ -24,6 +28,32 @@ class MapBox extends React.Component {
           center: [lng, lat],
           zoom: zoom
         });
+
+        var coordinates = [];
+        var currentMarkers = [];
+        // on a regular basis, add more coordinates from the saved list and update the map
+        var i = 0;
+        var timer = window.setInterval(function () {
+
+          // put markers on map
+          data.forEach((plane) => {
+            var marker = new mapboxgl.Marker()
+                                .setLngLat([plane.longitude, plane.latitude])
+                                .addTo(map);
+            coordinates.push([plane.longitude, plane.latitude]);
+          });
+
+          // if (i < coordinates.length) {
+          //   // clear prev markers 
+          //   if (currentMarkers !== null) {
+          //     for (var j = currentMarkers.length - 1; j >= 0; j--) {
+          //       currentMarkers[j].remove();
+          //     }
+          //   }
+              
+          //   i++;
+          // }
+        }, 1000);
     }
 
     render() {
