f0e6397bfdea7335559d2acdfc9eca02e21ee33b
franciscoxaviersp <45402958+franciscoxaviersp@users.noreply.github.com>
2021-05-19 19:56:05 +0100

    Update scripts

--- dubaidash/src/main/java/com/project/dubaidash/DubaidashApplication.java
+++ dubaidash/src/main/java/com/project/dubaidash/DubaidashApplication.java
@@ -13,7 +13,7 @@ public class DubaidashApplication {
 	@Bean
 	public RestTemplate getRestTemplate()
 	{
-		return new RestTemasdplate();
+		return new RestTemplate();
 	} 
 	public static void main(String[] args) {
 		SpringApplication.run(DubaidashApplication.class, args);
