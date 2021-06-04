366bbbc4559a728953209d30b563046b6876d98d
franciscoxaviersp <45402958+franciscoxaviersp@users.noreply.github.com>
2021-06-04 18:01:38 +0100

    Update tests.sh

--- jenkins/scripts/tests.sh
+++ jenkins/scripts/tests.sh
@@ -10,7 +10,7 @@ for i in ${!MODULES[@]}; do
 	cd ${MODULES[$i]}
 	rm -rf target
 	ls
-	mvn test
+	mvn test -Drun.arguments='--spring.datasource.url=jdbc:mysql://192.168.160.87:20006/dubaidash?useSSL=false,spring.kafka.consumer.bootstrap-servers=192.168.160.87:20002,spring.kafka.producer.bootstrap-servers=10.10.1.3:20002'
 
 	if [[ "$?" -ne 0 ]] ; then
 		rm -rf target
