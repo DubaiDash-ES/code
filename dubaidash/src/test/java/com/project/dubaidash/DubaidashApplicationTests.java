280216a25125692a751bea9733e4d12a3eca7813
tomasfilipe7 <tomasfilipe7@ua.pt>
2021-06-14 01:17:21 +0100

    exclusion commented

--- dubaidash/src/test/java/com/project/dubaidash/DubaidashApplicationTests.java
+++ dubaidash/src/test/java/com/project/dubaidash/DubaidashApplicationTests.java
@@ -1,59 +1,59 @@
-// package com.project.dubaidash;
+package com.project.dubaidash;
 
 
-// import org.junit.jupiter.api.Test;
-// import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-// import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.boot.test.web.client.TestRestTemplate;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
 
-// import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-// import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-// import com.project.dubaidash.controller.ProducerController;
-// import com.project.dubaidash.models.ParsingObject;
+import com.project.dubaidash.controller.ProducerController;
+import com.project.dubaidash.models.ParsingObject;
 
-// import org.springframework.kafka.KafkaException;
-// @SpringBootTest
-// @AutoConfigureMockMvc
-// class DubaidashApplicationTests {
-// 	@Autowired
-// 	private MockMvc mockMvc;
-// 	@Autowired
-// 	private ProducerController producerController;
-// 	@Autowired
-//     ParsingObject parsingObject;
+import org.springframework.kafka.KafkaException;
+@SpringBootTest
+@AutoConfigureMockMvc
+class DubaidashApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
+	@Autowired
+	private ProducerController producerController;
+	@Autowired
+    ParsingObject parsingObject;
 	
 
-// 	@Test
-// 	public void greetingShouldReturnDefaultMessage() throws Exception {
-// 		Assertions.assertEquals( parsingObject.parseObject(producerController.endpointAPI).getStatusCode(), HttpStatus.OK);
-// 	}
-// 	@Test
-// 	void contextLoads() throws Exception{
-// 		Assertions.assertNotNull(producerController);
-// 	}
+	@Test
+	public void greetingShouldReturnDefaultMessage() throws Exception {
+		Assertions.assertEquals( parsingObject.parseObject(producerController.endpointAPI).getStatusCode(), HttpStatus.OK);
+	}
+	@Test
+	void contextLoads() throws Exception{
+		Assertions.assertNotNull(producerController);
+	}
 
-// 	@Test
-// 	public void shouldReturnDefaultMessage() throws Exception
-// 	{
-// 		try
-// 		{
-// 			this.mockMvc.perform(get("/getstates")).andDo(print()).andExpect(status().isOk());
-// 		}
-// 		catch(org.springframework.web.util.NestedServletException e)
-// 		{
-// 			if(!(e.getCause() instanceof KafkaException))
-// 			{
-// 				throw new Exception(e.getCause());
-// 			}
-// 		}
+	@Test
+	public void shouldReturnDefaultMessage() throws Exception
+	{
+		try
+		{
+			this.mockMvc.perform(get("/getstates")).andDo(print()).andExpect(status().isOk());
+		}
+		catch(org.springframework.web.util.NestedServletException e)
+		{
+			if(!(e.getCause() instanceof KafkaException))
+			{
+				throw new Exception(e.getCause());
+			}
+		}
 		
-// 	}
+	}
 
-// }
+}
