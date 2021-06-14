b5341d0ef367ebc67b1b3255b6694faa37d679c1
tomasfilipe7 <tomasfilipe7@ua.pt>
2021-06-14 01:32:54 +0100

    clean

--- dubaidash/src/main/java/com/project/dubaidash/models/Producer.java
+++ dubaidash/src/main/java/com/project/dubaidash/models/Producer.java
@@ -1,14 +1,8 @@
 package com.project.dubaidash.models;
-import java.util.Collections;
-import java.util.Properties;
 
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.NewTopic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
